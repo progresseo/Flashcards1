8d506e936404aa9ed688c667b98c1847408fcd69
progresseo <94923126+progresseo@users.noreply.github.com>
2022-08-01 17:22:02 +0100

    Add Stacks MainMenu

--- Flashcards1/ManageStacks.cs
+++ Flashcards1/ManageStacks.cs
@@ -6,5 +6,42 @@ namespace Flashcards1
 {
     class ManageStacks
     {
-    }
+        public static void StacksMenu()
+        {
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("0 to Exit");
+                Console.WriteLine("A to Create Stack");
+                Console.WriteLine("B to Delete Stack");
+                Console.WriteLine("C to Update Stack");
+                Console.WriteLine("D to MainMenu");
+
+                string optionSelected = Console.ReadLine();
+
+                switch (optionSelected)
+                {
+                    case "0":
+                        closeApp = true;
+                        break;
+                    case "A":
+                        Console.WriteLine("test");
+                        break;
+                    case "B":
+                        Console.WriteLine("test");
+                        break;
+                    case "C":
+                        Console.WriteLine("test");
+                        break;
+                    case "D":
+                        MainMenu.Menu();
+                        break;
+                    default:
+                        Console.WriteLine("Please ensure you enter the letter exactly as seen.");
+                        break;
+                }
+
+
+            }
+        }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Flashcards1
 
-- Set up a gitignote file
+- Set up a gitignore file for the first time here so what is loaded to git is neat
+
+- https://docs.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/store-custom-information-config-file
+-Link shows how to set up App.config file
+
